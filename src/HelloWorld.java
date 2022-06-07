dbfafa1b57b7ba2cb1ba1722f3d0bdd8a97bd0fb
Justin English <justin.english39@gmail.com>
2022-06-07 09:05:03 -0500

    forgot to push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void  main(String[] args) {
         System.out.println("hello World!");
+
+
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userInput = Scanner.nextLine();
+        System.out.println("Hello, " + userInput);
     }
 }
 
